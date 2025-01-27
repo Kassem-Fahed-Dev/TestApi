60f2085a3eb8c26c2fb3793831b41b9e83527404
Kassem-Fahed-Dev <kassemfahed2003@gmail.com>
2025-01-27 01:40:37 -0800

    add /api/v1 route

--- server.js
+++ server.js
@@ -5,5 +5,7 @@ const app = express();
 app.get("/",(req,res)=>{
     res.send("hello Api");
 })
-
+app.get("/api/v1",(req,res) =>{
+    res.send("hello from server")
+})
 app.listen(3000,()=>console.log("server running"));
