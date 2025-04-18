d9f9fcb9e7fcb8211bf4acbdc72c1f9c5a26eec0
Kassem-Fahed-Dev <kassemfahed2003@gmail.com>
2025-04-18 12:44:30 +0300

    test

--- app.js
+++ app.js
@@ -42,6 +42,23 @@ app.use(express.static('public'));
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/auctions', auctionRouter);
 
+app.use('/',(req,res)=>{
+  const token = 'sddsdsdsss'
+  const cookieOptions = {
+    expires: new Date(
+      Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000,
+    ),
+    httpOnly: true,
+    secure: true,
+  };
+  //if (process.env.NODE_ENV === 'production') cookieOptions.secure = true;
+
+  // Remove password from output
+  res.cookie('jwt', token, cookieOptions).status(statusCode).json({
+    status: 'success',
+    token,
+  });
+})
 app.all('*', (req, res, next) => {
   next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
 });
